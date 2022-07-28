d734ace5c12a76151db10a19646dcb51d92e02aa
Aankirz <sahuankit453@gmail.com>
2022-07-28 19:47:02 +0530

    partially done

--- index.js
+++ index.js
@@ -1,15 +1,25 @@
+//Require express for setting up the express server
 const express=require('express');
+
 const path=require('path');
+//setting up the port number
 const port=8000;
 
+//importing the database
 const db=require('./config/mongoose');
+//importing the todo schema
 const Todo=require('./models/todo');
+//firing up the server
 const app=express();
 
+//setting up the view engine
 app.set('view engine','ejs');
+//setting up the path for the views
 app.set('views','./views')
 
+//To use encrypted data
 app.use(express.urlencoded());
+//To use the static files
 app.use(express.static('assets'));
 
 app.get('/',function(req,res){
@@ -25,7 +35,16 @@ Todo.find({},function(err,todos){
     })
   })
 })
-
+app.get('/delete-task',function(req,res){
+     let id=req.query.id;
+     Todo.findByIdAndDelete(id,function(err){
+            if(err){
+                console.log('Error in deleting the task');
+                return;
+            }
+            return res.redirect('back');
+     })
+})
 app.post('/create-todo',function(req,res){
      Todo.create({
         task:req.body.task,
--- models/todo.js
+++ models/todo.js
@@ -1,23 +1,25 @@
-// creating the db schema
+//require the library
 const mongoose=require('mongoose');
+
+// creating the db schema
 const toDoSchema=mongoose.Schema({
+    //task-- for Description
     task:{
+        type:String,
+        required:true
+    },
+    category:{
         type:String,
         required:true
     },
     date:{
         type:String,
-        required:true,
-    
-
-        
+        required:true
     },
-    // category:{
-    //     type:String,
-    //     required:true
-    // }
+    
 });
 
+// creating the model
 const Todo=mongoose.model('Todo',toDoSchema);
 
 //Finally export this model
